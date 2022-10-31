9bd12e41927bfba839abaf0e2e61ebae2fdbf837
jwo0o0 <jwo0o0358@gmail.com>
2022-10-31 19:59:46 +0900

    set proxy

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './App.css';
-
+import "./App.css";
 
 function App() {
-  const baseUrl = "http://localhost:8080"
+  const baseUrl = "http://localhost:8080";
 
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
 
   useEffect(() => {
     getTodos();
-  }, [])  // 한번만 실행됨
+  }, []); // 한번만 실행됨
 
-  async function getTodos() {   // 비동기 호출
+  async function getTodos() {
+    // 비동기 호출
     await axios
-      .get(baseUrl + '/todo')
+      .get("/todo")
       .then((response) => {
         setTodos(response.data);
       })
       .catch((error) => {
-        console.error(error)
-      })
+        console.error(error);
+      });
   }
 
   function insertTodo(e) {
@@ -29,65 +29,64 @@ function App() {
 
     const insertTodo = async () => {
       await axios
-            .post(baseUrl + '/todo', {
-              todoName : input
-            })
-            .then((response) => {
-              console.log(response.data)
-              setInput(""); // 초기화
-              getTodos();  // 추가한 데이터 다시 받아오기
-            })
-            .catch((error) => {
-              console.error(error);
-            })
-    }
+        .post("/todo", {
+          todoName: input,
+        })
+        .then((response) => {
+          console.log(response.data);
+          setInput(""); // 초기화
+          getTodos(); // 추가한 데이터 다시 받아오기
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
     insertTodo();
     console.log("할일이 추가됨");
   }
 
-  function updateTodo(id){
+  function updateTodo(id) {
     const updateTodo = async () => {
       await axios
-            .put(baseUrl + '/todo/' + id, {})
-            .then((response) => {
-              setTodos(
-              // 화면에서만 바꾸기 디비까지 접근하기 싫다
-              // 어차피 바뀐거 아니까!
-              todos.map((todo) =>
-                  todo.id === id ? { ...todo, completed: !todo.completed} : todo
-                )
-              )
-            })
-            .catch((error) => {
-              console.error(error);
-            })
-    }
+        .put("/todo/" + id, {})
+        .then((response) => {
+          setTodos(
+            // 화면에서만 바꾸기 디비까지 접근하기 싫다
+            // 어차피 바뀐거 아니까!
+            todos.map((todo) =>
+              todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+          );
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
     updateTodo();
-    
   }
 
   function deleteTodo(id) {
     const deleteTodo = async () => {
       await axios
-            .delete(baseUrl + '/todo/' + id, {})
-            .then((response) => {
-              // getTodos(); -> 쓸데없이 디비 한번 더 조회하므로 주석처리
-              setTodos(
-              // 화면에서만 바꾸기 디비까지 접근하기 싫다
-              // 어차피 바뀐거 아니까!
-                todos.filter((todo) => todo.id !== id)
-              )
-            })
-            .catch((error) => {
-              console.error(error);
-            })
-    }
+        .delete("/todo/" + id, {})
+        .then((response) => {
+          // getTodos(); -> 쓸데없이 디비 한번 더 조회하므로 주석처리
+          setTodos(
+            // 화면에서만 바꾸기 디비까지 접근하기 싫다
+            // 어차피 바뀐거 아니까!
+            todos.filter((todo) => todo.id !== id)
+          );
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    };
     deleteTodo();
   }
 
-  function changeText(e){
-    e.preventDefault();   // 다른행동 막기
-    setInput(e.target.value)  // input값을 setinput에 넣어준다
+  function changeText(e) {
+    e.preventDefault(); // 다른행동 막기
+    setInput(e.target.value); // input값을 setinput에 넣어준다
   }
 
   return (
@@ -96,28 +95,34 @@ function App() {
       <form onSubmit={insertTodo}>
         <label>
           Todo &nbsp;
-          <input type="text" required={true} value={input} onChange={changeText} />
+          <input
+            type="text"
+            required={true}
+            value={input}
+            onChange={changeText}
+          />
         </label>
         <input type="submit" value="Create" />
       </form>
-      {
-        todos
+      {todos
         ? todos.map((todo) => {
-          return (
-            <div className="todo" key={todo.id}>
-              <h3>
-                <label 
-                  className={todo.completed ? "completed" : null}
-                  onClick={() => updateTodo(todo.id)}>
-                  {todo.todoName}
-                </label>
-                <label onClick={() => deleteTodo(todo.id)}>&nbsp;&nbsp;&nbsp;❌</label>
-              </h3>
-            </div>
-          )
-        })
-         : null
-      }
+            return (
+              <div className="todo" key={todo.id}>
+                <h3>
+                  <label
+                    className={todo.completed ? "completed" : null}
+                    onClick={() => updateTodo(todo.id)}
+                  >
+                    {todo.todoName}
+                  </label>
+                  <label onClick={() => deleteTodo(todo.id)}>
+                    &nbsp;&nbsp;&nbsp;❌
+                  </label>
+                </h3>
+              </div>
+            );
+          })
+        : null}
     </div>
   );
 }
